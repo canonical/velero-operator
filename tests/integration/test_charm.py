e8103ced10531d6816e0308da65e7d4770625916
Kimonas Sotirchos <kimwnasptd@gmail.com>
2025-03-22 12:14:58 +0200

    fix: Unit and integration tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -6,6 +6,7 @@ import asyncio
 import logging
 from pathlib import Path
 
+from juju.model import Model
 import pytest
 import yaml
 from pytest_operator.plugin import OpsTest
@@ -14,6 +15,20 @@ logger = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./charmcraft.yaml").read_text())
 APP_NAME = METADATA["name"]
+
+def get_model(ops_test: OpsTest) -> Model:
+    """Return the Juju model of the current test.
+
+    Returns:
+        A juju.model.Model instance of the current model.
+
+    Raises:
+        AssertionError if the test doesn't have a Juju model.
+    """
+    model = ops_test.model
+    if model is None:
+        raise AssertionError("ops_test has a None model.")
+    return model
 
 
 @pytest.mark.abort_on_fail
@@ -24,12 +39,12 @@ async def test_build_and_deploy(ops_test: OpsTest):
     """
     # Build and deploy charm from local source folder
     charm = await ops_test.build_charm(".")
-    resources = {"httpbin-image": METADATA["resources"]["httpbin-image"]["upstream-source"]}
 
     # Deploy the charm and wait for active/idle status
+    model = get_model(ops_test)
     await asyncio.gather(
-        ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME),
-        ops_test.model.wait_for_idle(
+        model.deploy(charm, application_name=APP_NAME),
+        model.wait_for_idle(
             apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
         ),
     )
