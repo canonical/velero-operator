f1f97b539ba1ad4166b2db05b06e18e3ac05d847
Yurii Kondrakov <deezzir@gmail.com>
2026-03-09 20:24:13 -0400

    Update Copyrights to 2026 (#217)
    Signed-off-by: Deezzir <yurii.kondrakov@canonical.com>

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Canonical Ltd.
+# Copyright 2026 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 """Configuration for the charm."""
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Canonical Ltd.
+# Copyright 2026 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 from unittest.mock import ANY, MagicMock, PropertyMock, patch
